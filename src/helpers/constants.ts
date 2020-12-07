b05620083256f80c04f8780840fa192641c3d1c3
Tyler Gasperlin <gasperlint@gmail.com>
2020-12-06 20:09:21 -0800

    Add mongo env var

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,3 +1,14 @@
 export class EnvVariables {
+
+    /**
+     * A jwt signing key that is created in docker kubernetes and used in depl.yaml files
+     * Used for authentication
+     */
     static readonly JWT_KEY = 'JWT_KEY';
+
+    /**
+     * An environment variable that holds the url to a mongo database
+     */
+    static readonly MONGO_URI = 'MONGO_URI'
 }
+
