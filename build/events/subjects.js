c0ad3e3d59a7d056a3f8176ead9ba417efa40c8d
Tyler Gasperlin <gasperlint@gmail.com>
2021-06-20 07:42:05 -0700

    Updates

--- build/events/subjects.js
+++ build/events/subjects.js
@@ -5,4 +5,6 @@ var Subjects;
 (function (Subjects) {
     Subjects["TicketCreated"] = "ticket:created";
     Subjects["TicketUpdated"] = "ticket:updated";
+    Subjects["OrderCreated"] = "order:created";
+    Subjects["OrderCancelled"] = "order:Cancelled";
 })(Subjects = exports.Subjects || (exports.Subjects = {}));
